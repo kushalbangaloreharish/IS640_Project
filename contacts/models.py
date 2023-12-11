b8079e911d748d5622e8ced0f09011de5e7a0db5
Kushal_BangaloreHarish <kushal.bh599@gmail.com>
2023-12-11 13:26:37 -0800

    Initial Commit

---
+++ contacts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    notes = models.TextField()
+    time = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.name
