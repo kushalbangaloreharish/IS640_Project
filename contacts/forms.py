7d8ff230dcacb10803bb0a7237bc24592aef83ac
Kushal_BangaloreHarish <kushal.bh599@gmail.com>
2023-12-12 00:20:01 -0800

    create contact page

---
+++ contacts/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'phone', 'notes']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'notes': forms.Textarea(attrs={'class': 'form-control'}),
+        }
--- contacts/views.py
+++ contacts/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import Contact
+from .forms import ContactForm
 
 def contact_list(request):
     contacts = Contact.objects.all()
     return render(request, 'list.html', {'contacts': contacts})
 
 def contact_create(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_list')
+    else:
+        form = ContactForm()
+    return render(request, 'create.html', {'form': form})
+
+def contact_detail(request, pk):
+    pass
+
+def contact_edit(request, pk):
+    pass
+
+def contact_delete(request, pk):
     pass
